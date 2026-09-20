1a2635ab80c54d9851be5e8de1c1dcaeafe8df8d
RACHITHA K <rachitha102007@gmail.com>
2026-09-20 20:50:47 +0530

    added num

---
+++ num.java
@@ -0,0 +1,10 @@
+class example2{
+    public static void main(String[] args){
+        int num; 
+        num=100;
+        System.out.print("this is num: " + num);
+        num=num*2;
+        System.out.print(" the value of num * 2 is ");
+        System.out.print(num);
+    }
+}
